0010af9e1b048d769f01e53a6f5c3b50b8e5f304
Soumik Mukherjee <smsoumik1608@gmail.com>
2021-01-30 22:04:18 +0530

    Prime Factorisation using Sieve added

---
+++ primeFactorSieve.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void primeFactorSieve(int n){
+
+    int arr[n+1];
+
+    for(int i=0; i<=n; i++){
+        arr[i] = i;
+    }
+
+    for(int i=2; i<=n; i++){
+        if(arr[i] == i){
+            for(int j=i*i; j<=n; j+=i){
+                if(arr[j] == j){
+                    arr[j] = i;
+                }
+            }
+        }
+    }
+
+    while(n!=1){
+        cout<<arr[n]<<" ";
+        n /= arr[n]; 
+    }
+
+}
+
+int main(){
+
+    int n;
+    cout<<"ENTER THE NUMBER TO CHECK THE PRIME FACTORS:";
+    cin>>n;
+
+    primeFactorSieve(n);
+
+    return 0;
+}
